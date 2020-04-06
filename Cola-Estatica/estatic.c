7d23f8745cd0c92088f7938c2fc37dda1b789a04
Cristian Alexis Ochoa Garcia <crisocho3@gmail.com>
2020-04-06 17:22:20 -0500

    Entrega

---
+++ Cola-Estatica/estatic.c
@@ -0,0 +1,76 @@
+
+/**
+ \file estatic.c
+ \author Ochoa Garcia Cristian Alexis
+ \version 1.0
+ \date 01 / 04 /2020
+ \copyright GNU Public License v3.
+ Implementación de las funciones básicas
+ de una Cola Estatica de tamaño 5:
+ + insertar
+ + eliminar
+ + verificar si se encuestra vacia
+ + imprimir
+*/
+
+
+#include "estatic.h"
+
+int pila[TAM], cabeza=-1;
+
+void insertar(){
+	if(cabeza<4){  //Si pila no esta llena
+		printf("Introduzca el valor a meter a la pila: ");
+		printf("\n->");
+		scanf("%d", &pila[cabeza+1]);
+		cabeza++;
+		printf("El valor se ha introducido correctamente\n\n");
+		system("PAUSE");
+	}
+	else{
+		printf("Imposible meter datos, la pila esta llena\n\n");
+		system("PAUSE");
+	}
+}
+
+void eliminar(){
+	if(cabeza>-1){ //Si pila esta vacia
+		cabeza--;
+		
+		printf("Se ha eliminado el dato de la pila\n\nEl valor estimaod es %d\n\n", pila[cabeza+1]);
+		system("PAUSE");
+	}
+	else{
+		printf("Imposible sacar datos, la pila esta vacia\n\n");
+		system("PAUSE");
+	}
+}
+
+void pilavacia(){
+	if(cabeza==-1){
+		printf("La pila esta vacia\n\n");
+		system("PAUSE");
+	}
+	else{
+		printf("La pila NO esta vacia\n\n");
+		system("PAUSE");
+	}
+}
+
+void imprimir(){
+	int a=0;
+	if(cabeza>-1){	
+		printf("\t->");
+	
+		for(a=cabeza;a>-1;a--){
+			printf(" %d , ", pila[a]);
+		}
+		
+		system("PAUSE");
+		
+	}
+	else{
+		printf("La pila esta vacia\n\n");
+		system("PAUSE");
+	}
+}
---
+++ Cola-Estatica/estatic.h
@@ -0,0 +1,15 @@
+#ifndef __ESTATIC_H__
+#define __ESTATIC_H__
+
+#include<stdio.h>
+#include <stdlib.h>
+#define TAM 5	//Tamaño de la pila
+
+void imprimir();
+void pilavacia();
+void eliminar();
+void eliminar();
+void insertar();
+
+#endif
+
